1953aa27e76506595b2235e36fa79582d263d09b
Arif OKCAN <142164597+Etherfiend@users.noreply.github.com>
2024-10-08 16:51:56 +0300

    Add files via upload

---
+++ libft/main.c
@@ -0,0 +1,9 @@
+#include "libft.h"
+
+int	main(int argc, char **argv){
+	int c = atoi(argv[1]);
+	printf("%d\n", ft_isdigit('c'));
+	printf("%d\n", ft_isalpha('c'));
+	printf("%d\n", ft_isalnum('c'));
+	printf("%d\n", ft_isascii('c'));
+}
