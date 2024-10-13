54c109e3cb3957db80cc0475088822e3d0eb94ba
Etherfiend <aarifokcan@gmail.com>
2024-10-13 13:27:10 +0300

    add files via upload

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	dstlen;
+	size_t	srclen;
+
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	if(size <= dstlen)
+		return (srclen + size);
+	i = 0;
+	while((dstlen + i) < size - 1 && src[i])
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	dst[dstlen + i] = '\0';
+	return (dstlen + srclen);
+}
