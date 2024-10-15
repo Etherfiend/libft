49674802a8215c64e497b99ccecbcd00e9f58524
Etherfiend <aarifokcan@gmail.com>
2024-10-15 23:38:36 +0300

    add functions to libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -15,12 +15,20 @@
 
 # include <stdlib.h>
 
-# define INT_MIN -2147483648
-# define INT_MAX 2147483647
+# define FT_INT_MIN -2147483648
+# define FT_INT_MAX 2147483647
 # define SPACE 32
 
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 int		ft_atoi(const char *nptr);
 int		ft_isspace(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -29,6 +37,6 @@ void 	*ft_memset(void *b, int c, size_t len);
 size_t	ft_strlen(const char *c);
 void	ft_bzero(void *s, size_t n);
 void 	*ft_memcpy(void *dst, const void *src, size_t n);
-void 	*ft_memmove(void *dst, const void *src, size_t len);
+//void 	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
