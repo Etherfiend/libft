b23a8890a138cfea759189a561d26c332d2466e7
Etherfiend <aarifokcan@gmail.com>
2024-10-15 22:21:33 +0300

    modify libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -15,6 +15,12 @@
 
 # include <stdlib.h>
 
+# define INT_MIN -2147483648
+# define INT_MAX 2147483647
+# define SPACE 32
+
+int		ft_atoi(const char *nptr);
+int		ft_isspace(int c);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -22,7 +28,7 @@ int		ft_isascii(int c);
 void 	*ft_memset(void *b, int c, size_t len);
 size_t	ft_strlen(const char *c);
 void	ft_bzero(void *s, size_t n);
-void 	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-void 	*memmove(void *dst, const void *src, size_t len);
+void 	*ft_memcpy(void *dst, const void *src, size_t n);
+void 	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
