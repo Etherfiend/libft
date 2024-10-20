3694d86323e494b2ba85be3d1f640d9136f546a8
Muhammet Arif Okcan <muokcan@k2m17s05.42kocaeli.com.tr>
2024-10-20 13:13:01 +0300

    modify files

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -4,11 +4,12 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void *callocated;
 
-	if(nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0 || size > (size_t)-1)
 		return (NULL);
 	callocated = malloc(nmemb * size);
-	if(callocated)
-		ft_bzero(callocated, size * nmemb);
+	if (callocated == NULL)
+		return (NULL);
+	ft_bzero(callocated, size * nmemb);
 	return (callocated);
 }
 
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -10,3 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	int i;
+	int j;
+
+	if (little == NULL)
+		return ((char *)big);
+	i = 0;
+	while (big && i < len)
+	{
+		j = 0;
+		while(big[i + j] == little[j] && (i + j) < len)
+		{
+			if(little[j] == '\0')
+				return ((char *)big + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int	main()
+{
+		printf("%s\n", ft_strnstr("arifokcan", "if", 6));
+}
