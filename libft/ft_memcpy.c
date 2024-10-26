1f0d65d8fbc00f0d184f9b11c3df5413d914621a
Muhammet Arif Okcan <muokcan@k2m13s03.42kocaeli.com.tr>
2024-10-26 18:55:20 +0300

    modify files

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,36 +6,14 @@
 /*   By: muokcan <muokcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 14:35:33 by muokcan           #+#    #+#             */
-/*   Updated: 2024/10/18 18:03:11 by muokcan          ###   ########.fr       */
+/*   Updated: 2024/10/26 13:18:19 by muokcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t i;
-	char *to = dst;
-	const char *from = src;
-
-	if(to == from || n == 0)
-		return (to);
-	
-	i = 0;
-	while(n--)
-	{
-		to[i] = from[i];
-		i++;
-	}
-	return (to);
-}
-
-#include <stdio.h>
-
-int main()
-{
-	char arr[] = "helloworld";
-	ft_memcpy(arr + 5, arr, 2);
-	arr[7] = '\0';
-	printf("%s", arr);
+	ft_memmove(dest, src, n);
+	return (dest);
 }
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -12,7 +12,22 @@
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	
+	size_t i;
+
+	if(src == dest)
+		return (dest);
+
+	i = 0;
+	if(src < dest)
+		while(len--)	
+			((unsigned char *)dest)[len] = ((unsigned char *)src)[len];
+	else
+		while(len--)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+	return (dest);
 }
