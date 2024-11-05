a5dbe822c58bfc0d7f7c3b3600f4dcbe4b3477c9
Muhammet Arif Okcan <muokcan@k1m05s06.42kocaeli.com.tr>
2024-11-05 15:56:23 +0300

    modify files

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -25,19 +25,63 @@ static void	free_alloc(char **s)
 	free(s);
 }
 
-static int	wordlen(char *s, char sep)
+static unsigned int	wordlen(char *s, char sep)
 {
 	unsigned int	i;
+
+	i = 0;
 	while (s[i] && s[i] != sep)
 		i++;
 	return (i);
 }
 
-static int	wordcounter(char *s, char sep)
+static unsigned int	wordcounter(char *s, char sep)
 {
 	unsigned int	is_word;
 	unsigned int	wordcount;
 
 	is_word = 0;
 	wordcount = 0;
+	while (*s)
+	{
+		if (*s != sep && is_word == 0)
+		{
+			is_word = 1;
+			wordcount++;
+		}
+		else if (*s == sep && is_word == 1)
+			is_word = 0;
+		s++;
+	}
+	return (wordcount);
 }
+
+char	**ft_split(char const *s, char c)
+{
+	char			**res;
+	unsigned int	i;
+
+	if (!s)
+		return (NULL);
+	res = malloc(sizeof(char *) * (wordcounter((char *)s, c) + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (i < wordcounter((char *)s, c))
+	{
+		while (*s == c)
+			s++;
+		res[i] = malloc(sizeof(char) * (wordlen((char *)s, c) + 1));
+		if (!res[i])
+		{
+			free_alloc(res);
+			return (NULL);
+		}
+		ft_strlcpy(res[i], s, (wordlen((char *)s, c) + 1));
+		s += wordlen((char *)s, c);
+		i++;
+	}
+	res[i] = NULL;
+	return (res);
+}
+
