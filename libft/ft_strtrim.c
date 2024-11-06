7f3fd1b6c59a54eea5896b702bd0682255d5498d
Muhammet Arif Okcan <muokcan@k1m07s04.42kocaeli.com.tr>
2024-11-06 18:31:16 +0300

    add ft_strtrim.c

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char *ft_strtrim(char const *s1, char const *set)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	if (!s1 || !set)
+		return (NULL);
+	i = 0;
+	while (s1 && ft_strchr(set, s1[i]))
+		i++;
+	j = ft_strlen(s1);
+	while (i < j && ft_strrchr(set, s1[i]))
+		j--;
+	return (ft_substr(s1, i, ft_strlen(s1) - j + 1));
+}
